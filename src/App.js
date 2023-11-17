e51a90cba80309f8188eb9d4944c08021f5c3c04
couentine <quent.guillaume@gmail.com>
2023-11-17 10:44:42 -0800

    saving_lvl

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Picker } from '@react-native-picker/picker';
 import WordDisplay from './WordDisplay'; // Your WordDisplay component
 import { getWordOfTheDay } from './wordManager'; // Function to fetch the word of the day
-import LearningComponent from './LearningComponent';
 
 function App() {
   const [currentWord, setCurrentWord] = useState({});
@@ -13,6 +12,8 @@ function App() {
 
   useEffect(() => {
     loadLanguagePreference();
+    const savedLevel = loadLevelPreference();
+    setLevel(savedLevel); // Set the initial level based on saved preference
   }, []);
 
   useEffect(() => {
@@ -48,6 +49,26 @@ function App() {
     }
   };
 
+  // Function to save the selected level
+  const saveLevelPreference = (newLevel) => {
+    try {
+      localStorage.setItem('levelPreference', newLevel);
+    } catch (error) {
+      console.error('Failed to save level preference:', error);
+    }
+  };
+
+  // Function to load the selected level
+  const loadLevelPreference = () => {
+    try {
+      const savedLevel = localStorage.getItem('levelPreference');
+      return savedLevel || 'intermediate'; // Default to 'intermediate' if no level is set
+    } catch (error) {
+      console.error('Failed to load level preference:', error);
+      return 'intermediate'; // Default to 'intermediate' in case of error
+    }
+  };
+
   const scheduleNotification = () => {
     // Logic to schedule a daily notification with the word of the day
     // This should be implemented in your notificationManager
@@ -75,6 +96,7 @@ function App() {
   // Handler for level selection change
   const handleLevelChange = (newLevel) => {
     setLevel(newLevel);
+    saveLevelPreference(newLevel); // Save the selected level
   };
 
   return (
